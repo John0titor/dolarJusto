4b9ac480b20988455eee2515c7ceaa4e3492fbe8
John0titor <32824831+John0titor@users.noreply.github.com>
2020-07-27 23:11:28 -0300

    changes

--- init.R
+++ init.R
@@ -1,5 +1,10 @@
+# init.R
+
 my_packages <- c("quantmod","ecoseries","twitteR")
- install_if_missing <- function(p) {
- if(p %in% rownames(installed.packages())==FALSE){
- install.packages(p)}
- }
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
